91ccc59df15d174cf14b4359540afadf58b4ae45
rohinth96 <144210763+rohinth96@users.noreply.github.com>
2023-09-06 00:09:02 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+num =int(input("enter a number: "))
+factorial=1
+if num<0:
+  print("factorial cannot be found for negative numbers")
+elif num ==0:
+  print("factorial of 0 is 1")
+else:
+  for i in range(1,num+1):
+    factorial = factorial * i 
+print("factorial of",num,"is",factorial)
